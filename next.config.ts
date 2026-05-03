83f44eaa596813991602b72955d59d66c56f8275
浦和東岸町自治会 <urawa.higashikishi@gmail.com>
2026-05-03 22:30:48 +0900

    GitHub Actionsの設定を追加しデプロイ開始

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // リポジトリ名を指定することで、画像やリンクのパスを正しく通します
+  basePath: '/urawa-higashikishi-hp',
+  assetPrefix: '/urawa-higashikishi-hp',
 };
 
 export default nextConfig;
